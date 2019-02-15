469d928ef6b66ac29e88804b6a13bde661e82f69
Jose Roa <jg.roac@gmail.com>
2019-02-15 09:56:19 +0100

    feat: add not found and base routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Router } from '@reach/router';
 import Products from '../screens/Products';
+import NotFound from '../screens/NotFound';
 
 const Routes = () => (
   <Router>
+    <Products path="/" />
     <Products path="/categories/:categoryId" />
+    <NotFound default />
   </Router>
 );
 
---
+++ src/screens/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NotFound = () => <h1>Route Not Found (404)</h1>;
+
+export default NotFound;
